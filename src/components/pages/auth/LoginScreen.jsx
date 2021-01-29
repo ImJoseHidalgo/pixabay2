cf83685c3bccb24694646ee616e86e1a1ae376a2
imjosehidalgo <josehidalgo990@gmail.com>
2021-01-29 19:16:28 -0400

    login maquetado

--- src/components/pages/auth/LoginScreen.jsx
+++ src/components/pages/auth/LoginScreen.jsx
@@ -3,13 +3,18 @@ import { useDispatch } from 'react-redux';
 import { login } from '../../../actions/auth';
 import { useForm } from '../../../hooks/useForm';
 
+import googleIcon from '../../../icons/google.svg';
+import facebookIcon from '../../../icons/facebook.svg';
+import logo from '../../../icons/logo-Pixabay.png';
+import { Link } from 'wouter';
+
 const LoginScreen = () => {
 
   const dispatch = useDispatch();
 
   const [ formValues, handleInputChange ] = useForm({
-    email: 'ergun@gmail.com',
-    password: '123456'
+    email: '',
+    password: ''
   });
 
   const { email, password } = formValues;
@@ -22,15 +27,30 @@ const LoginScreen = () => {
   return (
     <div className='auth__loginScreen'>
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder='Email' name='email' value={email} onChange={handleInputChange} />
-        <input type="password" placeholder='Contraseña' name='password' value={password} onChange={handleInputChange} />
-        <button type='submit' >Login</button>
+        <div className="title">
+          <img src={logo} alt="pixabay-logo"/>
+        </div>
         <div className="auth__socials">
           <div className="auth__socials-google">
-            <h2>Login con Google</h2>
+            <div className="container">
+              <img src={googleIcon} alt="google icon"/>
+              <h2>Iniciar sesión con Google</h2>
+            </div>
+          </div>
+          <div className="auth__socials-facebook">
+            <div className="container">
+              <img src={facebookIcon} alt="facebook icon"/>
+              <h2>Iniciar sesión con Facebook</h2>
+            </div>
           </div>
         </div>
-        <p>Crear una cuenta nueva.</p>
+        <div className="text">
+          <h2>también puedes iniciar<br/>sesión con tu correo</h2>
+        </div>
+        <input type="email" placeholder='Email' name='email' value={email} onChange={handleInputChange} />
+        <input type="password" placeholder='Contraseña' name='password' value={password} onChange={handleInputChange} />
+        <button type='submit' >Iniciar Sesión</button>
+        <Link to='/'><p>Crear una cuenta nueva.</p></Link>
       </form>
     </div>
   )
